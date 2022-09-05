7322300e642c56e27b41d016bb2d49a73b190d49
junior <godualdo.junior-ext@viavarejo.com.br>
2022-09-05 15:47:10 -0300

    adiciona fixture e utiliza no command de login

--- cypress/support/Login/login-command.spec.js
+++ cypress/support/Login/login-command.spec.js
@@ -1,5 +1,5 @@
-Cypress.Commands.add("login", (email, senha) => {
-  cy.get('[data-test="login-email"] > .MuiInputBase-root > .MuiInputBase-input').type(email)
-  cy.get('[data-test="login-password"] > .MuiInputBase-root > .MuiInputBase-input').type(senha)
+Cypress.Commands.add("login", (usuario) => {
+  cy.get('[data-test="login-email"] > .MuiInputBase-root > .MuiInputBase-input').type(usuario.email)
+  cy.get('[data-test="login-password"] > .MuiInputBase-root > .MuiInputBase-input').type(usuario.senha)
   cy.get('[data-test="login-submit"]').click()
 })
